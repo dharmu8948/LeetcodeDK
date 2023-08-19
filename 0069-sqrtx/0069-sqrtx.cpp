1d5863d74c37980e7131eb526f86a36a7a3e39bd
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-19 12:23:29 +0530

    Time: 0 ms (100%), Space: 6 MB (42.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int binarySearch(int n) {
-        int s = 0;
-        int e = n;
-        long long int ans = -1;
-        while(s<=e) {
-            long long int mid = s + (e-s)/2;
-            long long int square = mid*mid;
-            if(square == n){
-                return mid;
-            }else if(square<n) {
-                ans = mid;
-                s = mid+1;
+        int start  = 0, end = n;
+        long long int res = -1;
+        while(start<=end) {
+            long long int mid = start + (end - start)/2;
+            long long int sRoot = mid*mid;
+            if(n == sRoot)
+            return mid;
+            else if(sRoot<n) {
+                res = mid;
+                start = mid+1;
             }else {
-                e = mid-1;
+               end = mid -1;
             }
+            
         }
-        return ans;
+        return res;
     }
     int mySqrt(int x) {
         return binarySearch(x);
