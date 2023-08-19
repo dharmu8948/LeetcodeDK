7a8a594785a12db719caec459ba69087463b6037
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-19 09:30:44 +0530

    Time: 13 ms (84.59%), Space: 17.9 MB (86.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // first approach
+        /*
         int sum = 0;
         int n = nums.size();
         for(int i=0;i<nums.size();i++) {
@@ -8,5 +10,17 @@ public:
         }
         int diff = (n*(n+1)/2) - sum;
         return diff;
+
+    */
+    // second approach using xor
+   // Dekho pehle array ke elements ka xor kr liye fir 0 se n tak xor kr liye..jo number missing hoga khud hi aa jayega.
+   int ele = 0;
+   for(int l: nums) {
+       ele^=l;
+   }
+   for(int i=0;i<=nums.size();i++) {
+       ele^=i;
+   }
+   return ele;
     }
 };
