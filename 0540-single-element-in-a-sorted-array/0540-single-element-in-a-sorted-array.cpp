e3aae17c3f99c412b88c3fde9ee4164ad60b8b75
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-20 10:33:27 +0530

    Time: 16 ms (92.4%), Space: 22.5 MB (8.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,43 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+
+        // first approach using XOR
+        /*
         int res = 0;
         for(int i=0;i<nums.size();i++) {
             res ^= nums[i];
         }
         return res;
+        */
+      // second approach using BS
+       if(nums.size() == 1){
+            return nums[0];
+        }
+
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+
+        if(nums[nums.size() - 1] != nums[nums.size() - 2]){
+            return nums[nums.size() - 1];
+        }
+
+        int low = 1,high = nums.size() - 2;
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid-1]){
+                return nums[mid];
+            }
+            // we are on the left half and element is on right half so eleminate left half
+            if((mid % 2 == 1 && nums[mid-1] == nums[mid]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 };
