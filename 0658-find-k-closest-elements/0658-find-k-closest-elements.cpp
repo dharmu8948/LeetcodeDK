bfb2e5be97fac589d95ab87925fce9703d993a5b
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-21 23:08:11 +0530

    Time: 35 ms (79.63%), Space: 31.1 MB (88.99%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+   int lowerBound(vector<int>& arr, int x) {
+       int start = 0, end = arr.size();
+       while(start<=end) {
+           int mid = start + (end - start)/2;
+           if(arr[mid] == x)
+           return mid;
+           else if(arr[mid]>x) {
+               end = mid - 1;
+           }else {
+               start = mid + 1;
+           }
+       }
+       return -1;
+   }
+    vector<int> bs_Method(vector<int>& arr, int k, int x) {
+        int high = lowerBound(arr,x);
+        int low = high-1;
+        while(k--) {
+            if(x-arr[low] > arr[high]-x) {
+                high++;
+            }else {
+                low--;
+            }
+        }
+         return vector<int>(arr.begin()+low, arr.begin()+high+1);
+    }
     vector<int> twoPtrMethod(vector<int>& arr, int k, int x) {
         // Using Binary Search
         int low = 0, high = arr.size()-1;
