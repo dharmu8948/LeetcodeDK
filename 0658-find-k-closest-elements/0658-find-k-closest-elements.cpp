e24327f90c6d5056a04828a1945ba785be4ac29a
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-21 22:45:58 +0530

    Time: 29 ms (89.53%), Space: 31.7 MB (70.86%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoPtrMethod(vector<int>& arr, int k, int x) {
+        // Using Binary Search
+        int low = 0, high = arr.size()-1;
+        while(high - low >= k) {
+            if(x-arr[low] > arr[high]-x) {
+                low++;
+            }else {
+                high--;
+            }
+        }
+        // now we have low and high point on correct position
+        vector<int> res;
+        for(int i = low;i<=high;i++) {
+            res.push_back(arr[i]);
+        }
+        return res;
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       return twoPtrMethod(arr, k,x);
+    }
+};
