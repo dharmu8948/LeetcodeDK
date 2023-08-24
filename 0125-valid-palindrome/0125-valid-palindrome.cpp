a63f56ffb679c09f8ce2343d21f36e5900d7cad6
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-24 08:57:54 +0530

    Time: 3 ms (90.11%), Space: 7.8 MB (20.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,45 @@
 class Solution {
+private:
+     bool validChar(char ch) {
+         if((ch>='a' && ch<='z') ||(ch>='A' && ch<='Z')|| (ch>='0' && ch<='9')) {
+             return true;
+         }
+         else return false;
+     }
+     char toLowerCase(char ch) {
+         if((ch>='a' && ch<='z') || (ch>='0' && ch<='9'))
+         return ch;
+         else {
+             char temp = ch-'A'+'a';
+             return temp;
+         }
+     }
+     bool chekPolindrome(string s) {
+         int start = 0, end = s.size()-1;
+         while(start<=end) {
+             if(s[start] != s[end])
+             return false;
+             else {
+                 start++;
+                 end--;
+             }
+         }
+         return true;
+     }
 public:
     bool isPalindrome(string s) {
-       int start=0;
-       int end=s.size()-1;
-       while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
-               start++;
-               end--;
+       
+       string temp = "";
+       // faltu character hata do 
+       for(int i=0;i<s.size();i++) {
+           if(validChar(s[i])) {
+               temp.push_back(s[i]);
            }
        }
-       return true;
+       // making into lowercase
+       for(int i=0;i<temp.size();i++) {
+           temp[i] = toLowerCase(temp[i]);
+       }
+       return chekPolindrome(temp);
     }
 };
