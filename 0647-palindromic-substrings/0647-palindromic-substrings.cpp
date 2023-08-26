5e59f1d69d957020084a717c21cf498c031707eb
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-26 23:14:40 +0530

    Time: 9 ms (56.45%), Space: 13.2 MB (41.84%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int expandAroundIndex(string s, int i, int j) {
+    int expandAroundIndex(string s, int left, int right) {
         int count = 0;
         // jab tak match kare tab tak count increment karte raho aur i derease(i--) and j increase(j++) kar do 
-        while(i>=0 && j<s.length() && s[i] == s[j]) {
+        while(left>=0 && right<s.length() && s[left] == s[right]) {
             count++;
-            i--;
-            j++;
+            left--;
+            right++;
         }
         return count;
     }
     int countSubstrings(string s) {
         int count = 0, n = s.length();
-        for(int i=0;i<n;i++) {
+        for(int center=0;center<n;center++) {
             // odd part check
-            int oddKaAns = expandAroundIndex(s,i,i);
+            int oddKaAns = expandAroundIndex(s,center,center);
             count += oddKaAns;
 
             // even part check
-            int evenKaAns = expandAroundIndex(s,i,i+1);
+            int evenKaAns = expandAroundIndex(s,center,center+1);
             count = count + evenKaAns;
         }
         return count;
