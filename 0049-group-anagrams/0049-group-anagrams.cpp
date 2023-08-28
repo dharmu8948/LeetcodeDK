36543ad68291a59db3a3f834b1ff7be6561cd1d3
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-08-28 20:59:04 +0530

    Time: 28 ms (83.94%), Space: 19.7 MB (75.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,int> mp;
-        vector<vector<string>> soln;
-        for(auto i:strs){
-            string temp=i;
-            sort(i.begin(),i.end());
-            if(mp[i])
-            {
-                soln[mp[i]-1].push_back(temp);
-            }
-            else{
-                soln.push_back({temp});
-                mp[i]=soln.size();
-            }
-        }
-        return soln;
+        // created a map to store sorted string vs all related string 
+       map<string, vector<string>> mp;
+       vector<vector<string>> ans;
+       // first sorting the current string and storing the current string
+       //Example: ["eat","tea","tan","ate","nat","bat"]
+       /*
+       step: 1:
+       */
+       for(int i=0;i<strs.size();i++) {
+           string s = strs[i];
+           sort(s.begin(), s.end());
+           mp[s].push_back(strs[i]);
+       }
+       for(auto it=mp.begin();it!=mp.end();it++) {
+           ans.push_back(it->second);
+       }
+       return ans;
     }
 };
