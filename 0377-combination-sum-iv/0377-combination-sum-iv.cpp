80b6894af8a4b79b1088b35991a11b551e733869
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-09-09 09:02:06 +0530

    Time: 0 ms (100%), Space: 6.6 MB (46.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
-   static const int MAX_TARGET = 1010; // Maximum possible target value
-    int dp[MAX_TARGET]; // Array to store computed results
+// first approach
+//    static const int MAX_TARGET = 1010; // Maximum possible target value
+//     int dp[MAX_TARGET]; // Array to store computed results
 
-    int countCombinations(vector<int> nums, int remainingTarget) {
-        // If the remaining target is 0, there's one valid combination.
-        if (remainingTarget == 0)
-            return 1;
+//     int countCombinations(vector<int> nums, int remainingTarget) {
+//         // If the remaining target is 0, there's one valid combination.
+//         if (remainingTarget == 0)
+//             return 1;
         
-        // If the remaining target becomes negative, it's not possible to reach it.
-        if (remainingTarget < 0)
-            return 0;
+//         // If the remaining target becomes negative, it's not possible to reach it.
+//         if (remainingTarget < 0)
+//             return 0;
         
-        // If the result for 'remainingTarget' is already computed, return it.
-        if (~dp[remainingTarget])
-            return dp[remainingTarget];
+//         // If the result for 'remainingTarget' is already computed, return it.
+//         if (~dp[remainingTarget])
+//             return dp[remainingTarget];
         
-        int currentCombinations = 0;
+//         int currentCombinations = 0;
         
-        // Iterate through the numbers in 'nums'.
-        for (int i = 0; i < nums.size(); i++) {
-            int currentNum = nums[i];
-            // recursively calculate combinations for the new target.
-            currentCombinations += countCombinations(nums, remainingTarget - currentNum);
-        }
+//         // Iterate through the numbers in 'nums'.
+//         for (int i = 0; i < nums.size(); i++) {
+//             int currentNum = nums[i];
+//             // recursively calculate combinations for the new target.
+//             currentCombinations += countCombinations(nums, remainingTarget - currentNum);
+//         }
         
-        // Store and return the computed result.
-        return dp[remainingTarget] = currentCombinations;
-    }
+//         // Store and return the computed result.
+//         return dp[remainingTarget] = currentCombinations;
+//     }
 
     int combinationSum4(vector<int>& nums, int target) {
-        // Initialize the 'dp' array with -1 to indicate uncomputed results.
-        for (int i = 0; i < MAX_TARGET; i++) {
-            dp[i] = -1;
-        }
+        // // Initialize the 'dp' array with -1 to indicate uncomputed results.
+        // for (int i = 0; i < MAX_TARGET; i++) {
+        //     dp[i] = -1;
+        // }
         
-        // Start the combination count calculation.
-        return countCombinations(nums, target);
+        // // Start the combination count calculation.
+        // return countCombinations(nums, target);
+        vector<unsigned> dp(target+1, 0);//Must be unsigned
+        dp[0]=1;
+        for(int i=1; i<=target; i++){
+            for (int x: nums){
+                if (i>=x)
+                    dp[i]+=dp[i-x];
+            }
+        }
+        return dp[target];
     }
 };
