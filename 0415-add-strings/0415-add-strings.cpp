950da6eacd59e926371d4b084d813087d7a86bf0
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2023-09-14 20:19:02 +0530

    Time: 6 ms (46.39%), Space: 6.9 MB (38.06%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    string addUsingRec(string num1, int p1, string num2, int p2, int carry=0) {
+    void addUsingRec(string &num1, int p1, string &num2, int p2, int carry, string& ans) {
 
         // base case
         if(p1<0 && p2<0) {
             if(carry != 0) {
-                return string(1,carry+'0');
+              ans.push_back(carry+'0');
             }
-            return "";
+            return ;
         }
         // ek case solve karo
         // extracting the digits from both string
@@ -16,16 +16,16 @@ public:
         int csum = n1 + n2 + carry;
         int digit = csum%10;
         carry = csum/10;
-        string ans = "";
         ans.push_back(digit+'0');
 
         // baki recursion sambhal lega
-        ans += addUsingRec(num1, p1-1, num2, p2-1,carry);
-        return ans;
+        addUsingRec(num1, p1-1, num2, p2-1,carry,ans);
     }
     string addStrings(string num1, string num2) {
-        string ans = addUsingRec(num1, num1.size()-1, num2, num2.size()-1);
+        string ans = "";
+        addUsingRec(num1, num1.size()-1, num2, num2.size()-1, 0, ans);
         reverse(ans.begin(), ans.end());
+        cout<<ans<<endl;
         return ans;
     }
 };
