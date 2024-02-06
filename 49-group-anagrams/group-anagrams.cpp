aafceb018f4c79e8410fd68effb23479a70056e1
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2024-02-06 23:37:59 +0530

    Time: 49 ms (20.16%) | Memory: 44.4 MB (5.00%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    std::array<int,256> hash(string s) {
+       std::array<int,256> hash ={0};
+       for(int i=0;i<s.length();i++) {
+           hash[s[i]]++;
+       } 
+       return hash;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // created a map to store sorted string vs all related string 
+       map<std::array<int, 256>, vector<string>> mp;
+       vector<vector<string>> ans;
+       for(auto str: strs) {
+           mp[hash(str)].push_back(str);
+       }
+       for(auto it = mp.begin();it != mp.end();it++) {
+           ans.push_back(it->second);
+       }
+      return ans; 
+    }
+};
