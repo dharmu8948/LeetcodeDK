acc71dcffd0c4c7871acaf714aa9f881eb1b0f48
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2024-03-13 11:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (15.68%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int leftSum = 0; // Initialize the sum of elements from 1 to x
-        int rightSum = 0; // Initialize the sum of elements from x to n
-        
-        // Iterate through all possible values of x
-        for(int i = 1; i <= n; i++) {
-            leftSum = i * (i + 1) / 2; // Calculate the sum of elements from 1 to i using arithmetic progression formula
-            rightSum = n * (n + 1) / 2 - i * (i - 1) / 2; // Calculate the sum of elements from i to n using arithmetic progression formula
-            
-            // Check if the sums are equal
-            if(leftSum == rightSum)
-                return i; // Return i as the pivot integer
+        int sum = 0;
+        for(int i=1;i<n+1;i++) {
+            sum += i;
         }
-        
-        return -1; // If no such integer exists, return -1
+        int sum1 =0;
+        int sum2 = 0;
+        for(int i=1;i<=n;i++) {
+            sum1 = sum1+i;
+            sum2 = i+sum-sum1;
+            if(sum1 == sum2)
+            return i;
+            else
+            sum2 = 0; 
+        }
+        return -1;
     }
 };
