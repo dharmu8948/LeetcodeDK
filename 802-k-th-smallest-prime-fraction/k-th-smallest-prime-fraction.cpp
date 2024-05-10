737f3a2578a893c372d7c59778b76d21ce20bf8f
Dharmendra Kumar <64011119+dharmu8948@users.noreply.github.com>
2024-05-10 18:56:58 +0530

    Time: 7 ms (90.33%) | Memory: 10.5 MB (100.00%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-         priority_queue<pair<double, pair<int, int>>, vector<pair<double, pair<int, int>>>, greater<>> minHeap;
-        int N = arr.size();
-        for (int i = 0; i < N; ++i) {
-            for (int j = i + 1; j < N; ++j) {
-                minHeap.push({(double)arr[i] / arr[j], {arr[i], arr[j]}});
+        int n = arr.size();
+        double left = 0, right = 1, mid;
+        vector<int> res;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            int j = 1, total = 0, num = 0, den = 0;
+            double maxFrac = 0;
+            for (int i = 0; i < n; ++ i) {
+                while (j < n && arr[i] >= arr[j] * mid) {
+                    ++j;
+                }
+                
+                total += n - j;
+
+                if (j < n && maxFrac < arr[i] * 1.0 / arr[j]) {
+                    maxFrac = arr[i] * 1.0 / arr[j];
+                    num = i; den = j;
+                }
+            }
+
+            if (total == k) {
+                res = {arr[num], arr[den]};
+                break;
+            }
+
+            if (total > k) {
+                right = mid;
+            } else {
+                left = mid;
             }
         }
-        
-        pair<int, int> result;
-        for (int i = 0; i < k; ++i) {
-            result = minHeap.top().second;
-            minHeap.pop();
-        }
-        
-        return {result.first, result.second};
+
+        return res;
     }
 };
