e9a0639d826091117e4ff68020b935c710499453
Dharmendra Kumar <dksharma8115@gmail.com>
2024-05-16 11:13:18 +0530

    Time: 9 ms (22.51%) | Memory: 8.6 MB (61.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    bool isChar(char ch) {
-        if((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch>=48 && ch<=57)) {
-            cout<<ch<<endl;
-            return true;
-        }
-        return false;
-    }
     bool isPalindrome(string s) {
-        string str="";
-        for(int i=0;i<s.size();i++) {
-            if(isChar(s[i]))
-                str += tolower(s[i]);
-        }
-        cout<<str<<" ";
-        for(int i=0, j= str.size()-1;i<str.size()/2;i++,j--) {
-            if(str[i] != str[j]) return false;
+        int i=0, j = s.size()-1;
+        while(i<j) {
+           if(!isalnum(s[i])) {
+            i++; continue;
+           }
+           if(!isalnum(s[j])) {
+            j--;continue;
+           }
+           if(tolower(s[i]) != tolower(s[j])) return false;
+           else {
+            i++;
+            j--;
+           }
         }
         return true;
     }
