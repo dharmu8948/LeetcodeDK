a1e0580e5134783f75f4d7c045307b586f7d5029
Dharmendra Kumar <dksharma8115@gmail.com>
2024-05-20 09:11:50 +0530

    Time: 4 ms (55.14%) | Memory: 8.4 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+     int helper(vector<int>& nums, int level, int currentXOR) {
+        // base condition
+        if (level == nums.size()) return currentXOR;
+        // creating include 
+        int include = helper(nums, level + 1, currentXOR ^ nums[level]);
+        // creating exclude
+        int exclude = helper(nums, level + 1, currentXOR);
+        
+        return include + exclude;
+    }
     int subsetXORSum(vector<int>& nums) {
-        int ors= 0;
-        for(int i=0;i<nums.size();++i){
-            ors = ors | nums[i];
-        }
-        return pow(2,nums.size()-1)*(ors);
+        // First create helper function, this function will help us to calculate
+        // when I'm including, when I'm not. The XOR of that array
+        return helper(nums, 0, 0); // nums, level, current XOR
     }
 };
