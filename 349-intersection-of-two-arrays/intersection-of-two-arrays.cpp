4358819db476f4aa166a1f787184130e770d9b99
Dharmendra Kumar <dksharma8115@gmail.com>
2024-05-20 18:03:46 +0530

    Time: 3 ms (82.12%) | Memory: 12.8 MB (54.80%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        set<int> myset(nums1.begin(), nums1.end());
-        set<int> myset2(nums2.begin(), nums2.end());
-        map<int, int> mp;
-        for(int num : myset) {
-            mp[num]++;
+       map<int, int> mp;
+       for(int n: nums1) {
+        mp[n]++;
+       }
+       vector<int> ans;
+       for(int n: nums2) {
+        if(mp.find(n) != mp.end() && mp[n] != -1) {
+            ans.push_back(n);
+            mp[n] = -1;
         }
-        for(int num : myset2) {
-           // cout<<mp[num]<<"   wdjsh  "<<num;
-            mp[num]++;
-            if(mp[num]>1) ans.push_back(num);
-        }
-       // cout<<endl;
-        return ans;
+       }
+       return ans;
     }
 };
