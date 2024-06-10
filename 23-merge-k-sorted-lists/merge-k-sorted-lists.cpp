d78708fb65e30b438bda58fac2ffe27ddd18d9f6
Dharmendra Kumar <dksharma8115@gmail.com>
2024-06-10 23:41:11 +0530

    Time: 16 ms (61.57%) | Memory: 16.8 MB (86.30%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,9 +8,18 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ class compare{
+    public:
+    bool operator()(ListNode* a, ListNode* b){
+    return a->val > b->val;
+
+    }
+ };
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // Approach 1
+        /*
         vector<int> arr;
         for(int i=0;i<lists.size();i++) {
             ListNode* temp = lists[i];
@@ -28,5 +37,42 @@ public:
             help = help->next;
         }
         return ans->next;
+        */
+
+        // Approach 2
+         if(lists.empty()){
+            return NULL;
+        }
+
+        
+     priority_queue<ListNode*, vector<ListNode*>, compare> minheap;
+    for(int i=0;i<lists.size();i++){
+        if(lists[i]!=NULL)
+    minheap.push(lists[i]);
+    }  
+    
+    ListNode* head=NULL;
+    ListNode* temp1;
+    while(!minheap.empty()){
+    ListNode* temp= minheap.top();
+   
+    if(head==NULL){
+    head=temp;
+     temp1=temp;}
+     else{
+     temp1->next=temp;
+     temp1=temp;}
+
+    minheap.pop();
+    if(temp->next!=NULL){
+        minheap.push(temp->next);
+    }
+
+
+ 
+    }
+    if(temp1!=NULL)
+    temp1->next=NULL;
+    return head; 
     }
 };
