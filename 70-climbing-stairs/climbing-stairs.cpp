ec8a3086009e51706c9ef68658574af70ae75f40
Dharmendra Kumar <dksharma8115@gmail.com>
2024-06-17 00:14:24 +0530

    Time: 2 ms (49.40%) | Memory: 7.1 MB (66.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp) {
-        if(n<0) return 0;
-        if(n==0) return 1;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = solve(n-1, dp) + solve(n-2, dp);
-        return dp[n];
-    }
     int climbStairs(int n) {
-       // tabulation method
-       vector<int> dp(n+1, -1);
-       return solve(n, dp);
+        int stairs[n+1];
+        stairs[0] = 1;
+        stairs[1] = 1;
+        for(int i=2;i<=n;i++) {
+            stairs[i] = stairs[i-1] + stairs[i-2];
+        }
+        return stairs[n];
     }
 };
