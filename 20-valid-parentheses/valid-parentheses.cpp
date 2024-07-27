abf3e70cbb5c0cb825916cc38606a8779aba7bcd
Dharmendra Kumar <dksharma8115@gmail.com>
2024-07-27 18:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
+
+        // pushing open brackets into stack ans poping if got close brackets
         for(int i=0;i<s.size();i++) {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                st.push(s[i]);
-            }
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
             else {
-                if(st.empty()) return false;
-                else if((s[i] == ')' && st.top() == '(') || (s[i] == '}' && st.top() == '{') || (s[i] == ']' && st.top() == '[')) st.pop();
-                else if((s[i] == ')' && st.top() != '(') || (s[i] == '}' && st.top() != '{') || (s[i] == ']' && st.top() != '[')) return false;
+                if(!st.empty()) {
+                    if((s[i] == ')' && st.top() == '(') || (s[i] == '}' && st.top() == '{') || (s[i] == ']' && st.top() == '[')) st.pop();
+                    else return false;
+                }else {
+                   if(s[i] == ']' || s[i] == '}' || s[i] == ')') return false; 
+                }
+                
             }
         }
         return st.empty();
