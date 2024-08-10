3953745880a467e0760553fc14f1f347906a519b
Dharmendra Kumar <dksharma8115@gmail.com>
2024-08-10 15:28:26 +0530

    Time: 10 ms (54.69%) | Memory: 16.9 MB (35.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,7 +1,40 @@
 class Solution {
 public:
-    // better solution with extra space
+    // Opimized soltion without extra space O(n*m)
     void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int col0 = 1;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(matrix[i][j] == 0) {
+                    // mark i-th row
+                    matrix[i][0] = 0;
+                    // mark j-th col
+                    if(j != 0)
+                      matrix[0][j] = 0;
+                    else
+                      col0 = 0;
+                }
+            }
+        }
+
+        // set values except 0-th row and 0-th col else it will hamper other value
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<m;j++) {
+                if(matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+            }
+        }
+        // set values at 0-th index for row and col
+        if(matrix[0][0] == 0) {
+            for(int j = 0;j<m;j++) matrix[0][j] = 0;
+        }
+         if(col0 == 0) {
+            for(int i = 0;i<n;i++) matrix[i][0] = 0;
+        }
+    }
+    // better solution with extra space (TIme Complexity: O(n*m + n*m) ~O(n*m), Space : max(O(m,n)))
+/*    void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
         vector<int> row(n,0);
@@ -22,6 +55,7 @@ public:
             }
         }
     }
+    */
 /*
    // brute force approach
     void setZeroes(vector<vector<int>>& matrix) {
