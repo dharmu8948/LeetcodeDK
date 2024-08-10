dd863cf7486c2358294cb544efc6792894cb04ba
Dharmendra Kumar <dksharma8115@gmail.com>
2024-08-10 13:49:58 +0530

    Time: 12 ms (40.05%) | Memory: 17 MB (19.21%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+   // brute force approach
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(matrix[i][j] == 0) {
+                    markRow(i,m, matrix);
+                    markCol(j,n, matrix);
+                }
+            }
+        }
+
+        // final iteration to make every -1 to 0
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(matrix[i][j] == -5689767) matrix[i][j] = 0;
+            }
+        } 
+    }
+    // mark the i-th row and assuming -5689767 is not in matrix
+    void markRow(int &i, int m, vector<vector<int>>& matrix) {
+        for(int j = 0;j<m;j++) {
+            if(matrix[i][j] !=0) matrix[i][j] = -5689767;
+        }
+    }
+    // mark the j-th col and assuming -5689767 is not in matrix
+    void markCol(int &j, int n, vector<vector<int>>& matrix) {
+        for(int i = 0;i<n;i++) {
+           if(matrix[i][j] !=0) matrix[i][j] = -5689767;
+        }
+    }
+
+};
