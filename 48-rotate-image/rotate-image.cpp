976685529881ce617359c01c4046f25ced250b77
Dharmendra Kumar <dksharma8115@gmail.com>
2024-08-11 13:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (44.64%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // Optimal Solution without extra space (in-place solution) T.C. O(n^2) S.C. - O(1)
+        int n = matrix.size();
+        // Step 1: Transpose the Matrix
+        for( int i = 0 ; i < n ; i++ ) {
+           for( int j = i ; j < n ; j++ ) {
+              swap(matrix[i][j], matrix[j][i]);
+           }
+        }
+        // Step 2: Reverse the column first to last second to second last and so on..
+         for( int i = 0 ; i < n ; i++ ) {
+          // reverse col
+          reverse(matrix[i].begin(), matrix[i].end());
+        }
+
+        /*
         // Brute force approach ( T.C. - O(n^2), S. C. - O(n^2))
          int n = matrix.size();
         vector<vector<int>> temp(n, std::vector<int>(n, 0));
@@ -15,5 +30,6 @@ public:
                 matrix[i][j] = temp[i][j];
             }
         }
+        */
     }
 };
