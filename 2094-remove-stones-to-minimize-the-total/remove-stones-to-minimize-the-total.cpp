35050f28823dfc4ea459f55611e6df46c35eb633
Dharmendra Kumar <dksharma8115@gmail.com>
2024-08-17 11:10:30 +0530

    Time: 460 ms (59.96%) | Memory: 108.9 MB (19.55%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
-        priority_queue<int> maxHeap;
-        for(int i=0;i<piles.size();i++) {
-            maxHeap.push(piles[i]);
-        }
-
-        while(k--) {
-            int topEle = maxHeap.top();
-            maxHeap.pop();
-            maxHeap.push(topEle - floor(topEle / 2));
-        }
+       priority_queue<int> maxH;
+       // adding values into max heap
+       for(int i=0;i<piles.size();i++) {
+        maxH.push(piles[i]);
+       }
+       // performing k operation to minimize the sum
+       while(k--) {
+        int num = maxH.top();
+        maxH.pop();
+        maxH.push(num - floor(num/2));
+       }
         int sum = 0;
-        while(!maxHeap.empty()) {
-            sum += maxHeap.top();
-            maxHeap.pop();
-        }
-        return sum;
+       // evaluating total sum now
+       while(!maxH.empty()) {
+        sum += maxH.top(); maxH.pop();
+       }
+
+       // returning minimized sum
+       return sum;
     }
 };
+
